f52f7bd51521e6a8a878ed184429dc5be5468cd5
Lap - Henry Niama <hsniama@utpl.edu.ec>
2021-10-12 10:24:38 -0500

    Añadiendo un "watch"
    - La función se llama "dev".

--- gulpfile.js
+++ gulpfile.js
@@ -2,13 +2,13 @@
 //realizar ciertas acciones.
 
 
-//utilizamos las dependencias de desarrollo instaladas en el package.json:
+// 1. Utilizamos las dependencias de desarrollo instaladas en el package.json:
 //(Va a node_modules y extrae la informacion y se guarda en la variable).
 //Esto es sintaxis de node.js
-const {src, dest} = require('gulp');//con la API de gulp se puede retornar multiples funciones
+const {src, dest, watch} = require('gulp');//con la API de gulp se puede retornar multiples funciones
 const sass = require('gulp-sass')(require('sass'));//copn gulp-sass solo podemos retornar una funcion.
 
-//Definicion de tareas:
+//2. Definicion de tareas:
 function css(done) {
     //1. Se debe identificar el archivo .scss a compilar (Se usa la funcion 'src' de gulp)
     src('src/scss/app.scss')//Le pasamos la ubicacion.
@@ -23,5 +23,18 @@ function css(done) {
     done();//para que la tarea finalize.
 }
 
-//Se hace disponibles las tareas para su ejecucion:
+//Creacion de funcion watch para que lea y guarde los cambios automaticamente
+function dev(done) {
+
+    //La funcion watch toma dos parametros, el primer es el archivo al que voy a escuchar o sea, 
+    //al que voy hacer cambios. Y cuando suceda cambios en dicho archivo, llamo a la funcion
+    //previamente creada 'css'.
+    watch('src/scss/app.scss', css);
+    done();
+}
+
+
+
+//3. Se hace disponibles las tareas para su ejecucion:
 exports.css = css;
+exports.dev = dev;
