f2a4a46b53fd0ffad8932961339cdaf007a70661
Lap - Henry Niama <hsniama@utpl.edu.ec>
2021-11-15 11:42:01 -0500

    Fijar la barra al dar scroll
    - Se edita navegacionFija() del app.js para controlar el scroll con .body-scroll.
    - En header.scss se edita el &.fijo.
    - En globales.scss se crea el &.body-scroll.

--- src/js/app.js
+++ src/js/app.js
@@ -103,14 +103,18 @@ function navegacionFija() {
     
     //Selecciono el header que va a quedar fijo
     const barra = document.querySelector('.header');
-
     const sobreFestival = document.querySelector('.sobre-festival');
+
+    const body = document.querySelector('body');
+
 
     window.addEventListener('scroll', function () {
         if( sobreFestival.getBoundingClientRect().top < 0){
             barra.classList.add('fijo');
+            body.classList.add('body-scroll');
         }else{
             barra.classList.remove('fijo');
+            body.classList.remove('body-scroll');
         }
     });
 }
